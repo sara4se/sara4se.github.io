54e87af11e8bbf2205e650b144634bcbb1954f37
sara <sara.ahumidi4@gmail.com>
2023-12-13 01:49:48 +0300

    changes in skills

--- lib/model/project_model.dart
+++ lib/model/project_model.dart
@@ -10,32 +10,32 @@ List<Project> projectList = [
   Project(
       'Tamrah',
       'Tamrah is an iOS App that use a Python model we trained and work on as a group in the SDAIA Bootcamp to detect the type of the Dates -Tamrah- fruit.',
-      'assets/images/alarm.jpg',
+      'assets/images/tamrah.png',
       'https://github.com/sara4se/Tamrah'),
   Project(
       'EcommercePage ',
-      'CUI AIDER is an extraordinary application that brings together augmented reality (AR), a robust student portal, intuitive class management, and a comprehensive GPA system, providing an all-encompassing solution for students.Using AR technology, CUI AIDER offers an immersive experience, allowing users to explore the complete university campus virtually. Through AR, students can visualize buildings, navigate with interactive directions, and gain a better understanding of their surroundings.',
-      'assets/images/cui.png',
-      'https://github.com/Hamad-Anwar/'),
+      '"EcommercePage" is a simple and user-friendly Flutter app that allows users to browse a selection of items on one page and add them to their shopping bag. Experience hassle-free shopping with an intuitive interface and straightforward navigation.',
+      'assets/images/profile.png',
+      'https://github.com/sara4se/EcommercePage'),
   Project(
       'iQoute Application',
       'Quotes: Scan your favorite quotes from books you read and save it ,So you can find it whenever you want in one place',
-      'assets/images/doctor.png',
+      'assets/images/Apps2.003.jpeg',
       'https://github.com/sara4se/iQuote'),
   Project(
       'Sign Language Classifier AI Application',
       'An app that helps children build and improve their characters so they can be ready for their educational journey, by including creative challenges and activities',
-      'assets/images/player.png',
+      'assets/images/profile.png',
       'https://github.com/sara4se/SignLanguageClassfierAI'),
   Project(
       'Kedu Application',
       'This repository contains a Flutter implementation of a Food Recipe app with a captivating introduction section and impressive animations. It also features a signup and signin page with animations, elegantly presented on a bottom sheet.',
-      'assets/images/recipe.png',
+      'assets/images/Apps2.001.jpeg',
       'https://github.com/Hamad-Anwar/Food-Recipe-App-Flutter'),
   Project(
     'LostIt Application',
     'LostIt : An application that allows users to post or navigate lost or found items in order to seek help from others by interacting with the posts through calls or chat.',
-    'assets/images/coffee.png',
+    'assets/images/lostit.jpeg',
     'https://github.com/sara4se/LostIt',
   ),
   Project(
@@ -47,6 +47,6 @@ List<Project> projectList = [
   Project(
       'WEVOLUNT Application',
         'An App that gather all the volunteering opportunities and divide them into categories based on interest',
-      'assets/images/task.png',
+      'assets/images/Apps2.002.jpeg',
       'https://github.com/perfangx/weVolunteer'),
 ];
